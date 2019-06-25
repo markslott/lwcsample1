ec6d74ffacb4a14f056ec8bf1669c1bf7dfae149
Mark Lott <mlott@mlott-ltm5.internal.salesforce.com>
2019-06-25 13:53:03 -0500

    init

---
+++ force-app/main/default/lwc/contactMapper/contactMapper.js
@@ -0,0 +1,47 @@
+import { LightningElement, track, wire } from 'lwc';
+import getContacts from '@salesforce/apex/ContactMapperController.getContacts';
+
+export default class ContactMapper extends LightningElement {
+
+    @track data;
+    @track mapMarkers = [];
+
+    @wire(getContacts, {}) salesforceContacts; 
+
+    xform = contacts => {
+        var markers = [];
+        for (let contact of contacts) {
+            let marker = {
+                location : {
+                    City : contact.MailingCity,
+                    Country : contact.MailingCountry,
+                    PostalCode : contact.MailingPostalCode,
+                    State : contact.MailingState,
+                    Street : contact.MailingStreet
+                },
+                title : contact.FirstName + ' ' + contact.LastName,
+                icon : 'standard:contact' 
+            }
+            markers.push(marker);
+        }
+        return markers;
+    }
+
+    getSelectedName = event => {
+        const selectedRows = event.detail.selectedRows;
+        this.mapMarkers=this.xform(selectedRows);
+    }
+
+    /*async connectedCallback() {
+        const data = await dataHelper();
+        this.data = data;
+    }*/
+
+    columns = [
+        { label: 'First Name', fieldName: 'FirstName' },
+        { label: 'Last Name', fieldName: 'LastName' },
+        { label: 'Phone', fieldName: 'Phone', type: 'phone' },
+        { label: 'Email', fieldName: 'Email' },
+        { label: 'Zip Code', fieldName: 'MailingPostalCode' },
+    ];
+}
